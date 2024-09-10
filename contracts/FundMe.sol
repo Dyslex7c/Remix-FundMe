190d121580182f68937100b653fdffc1122954c9
Dyslex7c <polynsominiac@gmail.com>
2024-09-10 20:06:07 +0530

    Second commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import { PriceConverter } from "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        //require(msg.sender == i_owner, "Sender is not i_owner!");
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+}
